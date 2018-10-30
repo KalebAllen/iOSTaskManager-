ca337b65c439ab48825b6c30b6c79ce3dd930f8b
Kaleb Allen <allen.henry.kaleb@.com>
2018-10-30 14:30:40 -0500

    inital Commit

---
+++ iOSTaskManager/ViewControllers/AddTaskViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddTaskViewController.swift
+//  iOSTaskManager
+//
+//  Created by Kaleb Allen on 10/30/18.
+//  Copyright © 2018 Kaleb Allen. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    @IBOutlet weak var TitleTaskTextFeild: UITextField!
+    @IBOutlet weak var DiscriptionTextFeild: UITextField!
+    @IBOutlet weak var SubmitButtion: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOSTaskManager/ViewControllers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  iOSTaskManager
+//
+//  Created by Kaleb Allen on 10/30/18.
+//  Copyright © 2018 Kaleb Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func AddTask(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
